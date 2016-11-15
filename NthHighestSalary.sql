a6003559c4a4642068c1d2ddc601ef0d80a10612
Delheswar PJ <delheswar4u@gmail.com>
2016-11-15 17:00:42 +0530

    Update NthHighestSalary.sql

--- NthHighestSalary.sql
+++ NthHighestSalary.sql
@@ -30,7 +30,7 @@ Insert into EMP2 (EMP2NO,ENAME,JOB,MGR,HIREDATE,SAL,COMM,DEPTNO) values (7876,'A
 Insert into EMP2 (EMP2NO,ENAME,JOB,MGR,HIREDATE,SAL,COMM,DEPTNO) values (7900,'JAMES','CLERK',7698,to_date('03-DEC-81','DD-MON-RR'),950,null,30);
 Insert into EMP2 (EMP2NO,ENAME,JOB,MGR,HIREDATE,SAL,COMM,DEPTNO) values (7369,'SMITH','CLERK',7902,to_date('17-DEC-80','DD-MON-RR'),800,null,20);
 
-----------------------------------------------------------------------------
+/*----------------------------------------------------------------------------*/
 SELECT EMP2NO,
   ENAME,
   JOB,
@@ -41,31 +41,32 @@ SELECT EMP2NO,
   DEPTNO
 FROM EMP2
 ORDER BY SAL DESC;
-----------------------------------------------------------------------------   
+/*----------------------------------------------------------------------------*/
 
 |EMP2N0	|ENAME	|JOB		|MGR	|HIREDATE	|SAL	|COMM	|DEPTNO	|
-|-------|-------|-----------|-------|-----------|-------|-------|-------|
-|7839	|KING	|PRESIDENT	|		|17-NOV-81	|5000	|		|10		|
-|7902	|FORD	|ANALYST	|7566	|03-DEC-81	|3000	|		|20		|
-|7788	|SCOTT	|ANALYST	|7566	|09-DEC-82	|3000	|		|20		|
-|7566	|JONES	|MANAGER	|7839	|02-APR-81	|2975	|		|20		|
-|7698	|BLAKE	|MANAGER	|7839	|01-MAY-81	|2850	|		|30		|
-|7782	|CLARK	|MANAGER	|7839	|09-JUN-81	|2450	|		|10		|
-|7499	|ALLEN	|SALESMAN	|7698	|20-FEB-81	|1600	|300	|30		|
-|7844	|TURNER	|SALESMAN	|7698	|08-SEP-81	|1500	|0		|30		|
-|7934	|MILLER	|CLERK		|7782	|23-JAN-82	|1300	|		|10		|
-|7521	|WARD	|SALESMAN	|7698	|22-FEB-81	|1250	|500	|30		|
-|7654	|MARTIN	|SALESMAN	|7698	|28-SEP-81	|1250	|1400	|30		|
-|7876	|ADAMS	|CLERK		|7788	|12-JAN-83	|1100	|		|20		|
-|7900	|JAMES	|CLERK		|7698	|03-DEC-81	|950	|		|30		|
-|7369	|SMITH	|CLERK		|7902	|17-DEC-80	|800	|		|20		|
+|-------|-------|---------------|-------|---------------|-------|-------|-------|
+|7839	|KING	|PRESIDENT	|	|17-NOV-81	|5000	|	|10	|
+|7902	|FORD	|ANALYST	|7566	|03-DEC-81	|3000	|	|20	|
+|7788	|SCOTT	|ANALYST	|7566	|09-DEC-82	|3000	|	|20	|
+|7566	|JONES	|MANAGER	|7839	|02-APR-81	|2975	|	|20	|
+|7698	|BLAKE	|MANAGER	|7839	|01-MAY-81	|2850	|	|30	|
+|7782	|CLARK	|MANAGER	|7839	|09-JUN-81	|2450	|	|10	|
+|7499	|ALLEN	|SALESMAN	|7698	|20-FEB-81	|1600	|300	|30	|
+|7844	|TURNER	|SALESMAN	|7698	|08-SEP-81	|1500	|0	|30	|
+|7934	|MILLER	|CLERK		|7782	|23-JAN-82	|1300	|	|10	|
+|7521	|WARD	|SALESMAN	|7698	|22-FEB-81	|1250	|500	|30	|
+|7654	|MARTIN	|SALESMAN	|7698	|28-SEP-81	|1250	|1400	|30	|
+|7876	|ADAMS	|CLERK		|7788	|12-JAN-83	|1100	|	|20	|
+|7900	|JAMES	|CLERK		|7698	|03-DEC-81	|950	|	|30	|
+|7369	|SMITH	|CLERK		|7902	|17-DEC-80	|800	|	|20	|
    
    
-----------------------------------------------------------------------------
+/*------------------------------OUTPUT EXEPCTED--------------------------------*/
+
 |EMP2N0	|ENAME	|JOB		|MGR	|HIREDATE	|SAL	|COMM	|DEPTNO	|
-|-------|-------|-----------|-------|-----------|-------|-------|-------|
-|7902	|FORD	|ANALYST	|7566	|03-DEC-81	|3000	|		|20		|
-|7788	|SCOTT	|ANALYST	|7566	|09-DEC-82	|3000	|		|20		|
+|-------|-------|---------------|-------|---------------|-------|-------|-------|
+|7902	|FORD	|ANALYST	|7566	|03-DEC-81	|3000	|	|20	|
+|7788	|SCOTT	|ANALYST	|7566	|09-DEC-82	|3000	|	|20	|
 
 -- USING WITH AS
 
